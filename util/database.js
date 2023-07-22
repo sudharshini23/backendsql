7266c7c36951accf96a4703bf4e35367c12575a4
sudharshini23 <sudarsh25@gmail.com>
2023-07-22 10:44:47 +0530

    After Sequelize basics to connect

--- util/database.js
+++ util/database.js
@@ -1,10 +1,23 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'LearnSQL@#23'
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: 'LearnSQL@#23'
+// });
+
+// module.exports = pool.promise()
+
+// ---------------------------------------------
+
+// Import Sequelize constructor/class
+const Sequelize = require('sequelize');
+
+// Create a new instance by calling the constructor
+const sequelize = new Sequelize('node-complete', 'root', 'LearnSQL@#23', {
+    dialect: 'mysql', 
+    host: 'localhost'
 });
 
-module.exports = pool.promise()
+module.exports = sequelize;
